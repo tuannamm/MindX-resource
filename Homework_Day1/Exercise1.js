ee73507f905b9d167075bb599f8a90f6c309d15c
Tuan Nam <nttnnam@gmail.com>
2022-06-17 15:23:52 +0700

    homework

---
+++ Homework_Day1/Exercise1.js
@@ -0,0 +1,7 @@
+/* Bài 1 Humanize Format
+Hãy giúp chúng tôi thiết lập một Function Javascript sao cho: 
+dữ liệu nhận vào là một **`number` và dữ liệu đầu ra là một chuỗi thõa mãn yêu cầu sau:**
+1 ⇒ "1st"
+32 ⇒ "32nd"
+208 ⇒ "208th"
+403 ⇒ "403rd" */
