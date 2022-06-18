0f3dab11aec36a8b7a4bebbcc75437e62cf89e1c
Tuan Nam <nttnnam@gmail.com>
2022-06-18 09:52:57 +0700

    homework

--- Homework_Day1/excercise2.js
+++ Homework_Day1/excercise2.js
@@ -6,5 +6,15 @@ Hàm sẽ nhận 2 tham số đầu vào: chuỗi **`str`** cần repeat
 và count là số lần `**repeat**` */
 
 function repeatString(str, count) {
-    
+  if (typeof str !== "string" || typeof count !== "number") return false;
+
+  let newStr = "";
+
+  for (let i = 0; i < count; i++) {
+    newStr += str;
+  }
+
+  return newStr;
 }
+
+console.log(repeatString("Duyn", 3));
--- Homework_Day1/tempCodeRunnerFile.js
+++ Homework_Day1/tempCodeRunnerFile.js
@@ -1,26 +1,20 @@
-/* Bài 1 Humanize Format
-Hãy giúp chúng tôi thiết lập một Function Javascript sao cho: 
-dữ liệu nhận vào là một **`number` và dữ liệu đầu ra là một chuỗi thõa mãn yêu cầu sau:**
-1 ⇒ "1st"
-32 ⇒ "32nd"
-208 ⇒ "208th"
-403 ⇒ "403rd" */
+/* Bài 2 Repeat String
 
-function humanizeFormat(number) {
-  if (typeof number !== "number") return false;
+Viết hàm Javascript thõa mãn yêu cầu sau.
 
-  let lastDigitStr = String(number).slice(-1);
-  let lastDigitNumber = Number(lastDigitStr);
+Hàm sẽ nhận 2 tham số đầu vào: chuỗi **`str`** cần repeat 
+và count là số lần `**repeat**` */
 
-  if (lastDigitNumber === 1) return `${number}st`;
-  if (lastDigitNumber === 2) return `${number}nd`;
-  if (lastDigitNumber === 3) return `${number}rd`;
+function repeatString(str, count) {
+    if (typeof str !== 'string' || typeof count !== 'number') return false;
 
-  return `${number}th`;
+    let newStr = '';
+
+    for (let i = 0; i < count; i++) {
+        newStr += str;
+    }
+
+    return newStr;
 }
 
-console.log(humanizeFormat(111));
-console.log(humanizeFormat(322));
-console.log(humanizeFormat(393));
-console.log(humanizeFormat(198));
-
+console.log(repeatString("Duyn", 3));
