2ab13f121677087c72dcc4bc6855046d7290d8e3
Tuan Nam <nttnnam@gmail.com>
2022-06-21 11:12:31 +0700

    homework

---
+++ Homework_Day1/problem1.js
@@ -0,0 +1,16 @@
+/*Write a function that takes an array of users that satisfy the following requirement.
+Use array map make an array of the names in <h1></h1> and the ages in <h2></h2>.*/
+
+const users = [
+  { name: "Angelina Jolie", age: 8 },
+  { name: "Eric Jones", age: 2 },
+  { name: "Paris Hilton", age: 5 },
+  { name: "Kayne West", age: 16 },
+  { name: "Bob Ziroll", age: 100 },
+];
+
+function problem1(arr) {
+  if (!Array.is(arr)) return false;
+
+  let res = arr.map();
+}
